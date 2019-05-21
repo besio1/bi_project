a27826ae9e64e044fba42c307f7fb68a80f27565
Unknown <musabgamal.elkour@students.bfh.ch>
2019-05-21 18:58:31 +0200

    modified code variable + some corrections

--- master_Musab2014.R
+++ master_Musab2014.R
@@ -15,45 +15,46 @@ library(choroplethr)
 setwd("C:/dev/bi_project")
 
 #import data with help of the readr package and reads comma delimited files
-hc <- read.csv("PlanAttributes.csv", stringsAsFactors = FALSE)
+pa <- read.csv("PlanAttributes.csv", stringsAsFactors = FALSE)
+
 
 #prints all the data in the columns which are in the business year 2014
 #Just for checking the access to the data 
-hc <- subset(hc, BusinessYear == "2014")
-print(subset(hc, BusinessYear == "2014"))
+#subset and filter the data just for the buisness year 2014
+pa <- subset(pa, BusinessYear == "2014")
+print(subset(pa, BusinessYear == "2014"))
 
-#Just for checking the access to the data 
-#prints all the data in the columns which the DentalOnlyPlan is equal "No" 
-hc <- subset(hc, DentalOnlyPlan == "No")
-print(subset(hc, DentalOnlyPlan == "No"))
 
 #A quick glimpse and then some data cleaning of SBCHavingDiabetesCoinsurance (The dollar amount of the coinsurance for the sample SBC scenario of having diabetes)
 #Coinsurance = Eine Mitversicherung ist die Beteiligung mehrerer Versicherungsunternehmen an der Versicherung desselben Risikos
 #SBC= Summary of Benefits and Coverage Provides Clear and Consistent Information == allowing employers and employees to make apples-to-apples comparisons among plans, understand what is covered and what it costs
-head(hc$SBCHavingDiabetesCoinsurance, 100)
+head(pa$SBCHavingDiabetesCoinsurance, 100)
 
 # replace all "," with " " AND all "\\$" with " " in SBCHavingDiabetesCoinsurance
-hc$SBCHavingDiabetesCoinsurance<- gsub('\\$', '', hc$SBCHavingDiabetesCoinsurance)
-hc$SBCHavingDiabetesCoinsurance<- gsub(',', '', hc$SBCHavingDiabetesCoinsurance)
+pa$SBCHavingDiabetesCoinsurance<- gsub('\\$', '', pa$SBCHavingDiabetesCoinsurance)
+pa$SBCHavingDiabetesCoinsurance<- gsub(',', '', pa$SBCHavingDiabetesCoinsurance)
 # assign (and convert to numeric) the column of SBCHavingDiabetesCoinsurance
-hc$coinsurance <- as.numeric(hc$SBCHavingDiabetesCoinsurance)
+pa$coinsurance <- as.numeric(pa$SBCHavingDiabetesCoinsurance)
 # fill the blank cells with 0
-hc$coinsurance[is.na(hc$coinsurance)] <- 0
-head(hc$coinsurance, 100)
+pa$coinsurance[is.na(pa$coinsurance)] <- 0
+head(pa$coinsurance, 100)
+
+#subset and filter all coinsurances which equals 0 (remove all 0 values)
+pa <- subset(pa, coinsurance != 0)
 
 
-
-counts <- table(hc$coinsurance)
+##set data of coinsurance in a table for the barplot 
+counts <- table(pa$coinsurance)
 #barplot of coinsurance
-barplot(counts, main="The coinsurance for the sample SBC scenario of having diabetes",
-        xlab="Dollar Amount", ylab="Counts")
+barplot(counts, main="Deckungskosten der Mitversicherung für Diabeteserkrankte in 2014",
+        xlab="Dollar Amount", ylab="Counts", col="green")
 
 # plot histogram of coinsurance dollar amount (x axis) and count in (y axis)
-ggplot(hc, aes(x = hc$coinsurance)) + geom_histogram(color="green")
+ggplot(pa, aes(x = pa$coinsurance)) + geom_histogram(col="black", fill="green")
 
 
 #list aggregation of the mean of the coinsurance with the statecode and print it 
-df <- aggregate(hc$coinsurance, list(hc$StateCode), mean)
+df <- aggregate(pa$coinsurance, list(pa$StateCode), mean)
 df
 
 #Set the title of the printed aggregated list above with new labels and print it 
@@ -64,19 +65,19 @@ df
 # converts states abbreviations
 source("function_stateFromLower.R")
 #calling the states from the function stateFromLower 
-df$region<-stateFromLower(df$Group.1)
+df$region<-stateFromLower(df$Group1)
 df$value <- df$overall
 
 #get summary of the states, coinsurance and region (Length, Median, mean...)
 summary(df)
 
 #Subset of all the coinsurances above 0 
-coinsurance <- subset(hc, coinsurance > 0)
+coinsurance <- subset(pa, coinsurance > 0)
 
 
 #------------------------choro Map creation -------------------------------------
 #Aggregate the coinsurance with statecodes 
-df <- aggregate(hc$coinsurance, list(hc$StateCode), mean)
+df <- aggregate(pa$coinsurance, list(pa$StateCode), mean)
 #calling the states from the function stateFromLower
 # Group.i for by[[i]]. -> in this case first group "Group.1" is group of all states
 df$region<-stateFromLower(df$Group.1)
@@ -86,14 +87,14 @@ df$value <- df$x
 choro = StateChoropleth$new(df)
 
 # set title to the new choro map
-choro$title = "    The coinsurance for the sample SBC scenario of having diabetes"
+choro$title = "    Deckungskosten der Mitversicherung für Diabeteserkrankte in 2014"
 
 # The number of colors to use on the map. A value of 0 uses a divergent scale
 choro$set_num_colors(1)
 
 # These functions are useful for converting hand-designed `sequential' or `diverging' color 
 # here green levels set 
-myPalette <- colorRampPalette(brewer.pal(9, "Greens"))
+myPalette <- colorRampPalette(brewer.pal(9, "Blues"))
 
 #fill value and border color 
 choro$ggplot_polygon = geom_polygon(aes(fill = value), color = "black")
@@ -126,7 +127,7 @@ class(states)
 names(states)
 
 #a basic map with just the outline of the states
-m <- leaflet(states) %>%
+map <- leaflet(states) %>%
   setView(-96, 37.8, 4) %>%
   addProviderTiles("MapBox", options = providerTileOptions(
     id = "mapbox.light",
@@ -134,19 +135,19 @@ m <- leaflet(states) %>%
 
 #We've saved the basic basemap as a separate variable m so we can easily iterate on the addPolygons call 
 #call addPolygons with no additional arguments -- To add uniform polygons with default styling
-m %>% addPolygons()
+map %>% addPolygons()
 
 #First, we'll define the bins. This is a numeric vector that defines the boundaries between intervals 
 bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf)
 
 #Then, we'll call colorBin to generate a palette function that maps the RColorBrewer "YlOrRd" colors to our bins.
-pal <- colorBin("YlOrRd", domain = states$density, bins = bins)
+pal <- colorBin("YlGnBu", domain = pa$coinsurance, bins = bins)
 
 
 #Adding Color to the map 
-#Finally, we'll modify addPolygons to use the palette function and the density values to generate a vector of colors for fillColor, and also add some other static style properties.
-m %>% addPolygons(
-  fillColor = ~pal(density),
+#Finally, we'll modify addPolygons to use the palette function to generate a vector of colors for fillColor, and also add some other static style properties.
+map %>% addPolygons(
+  fillColor = ~pal(pa$coinsurance),
   weight = 2,
   opacity = 1,
   color = "white",
@@ -155,8 +156,8 @@ m %>% addPolygons(
 
 #Adding interaction 
 #make the polygons highlight as the mouse passes over them. The addPolygon function has a highlight argument that makes this simple
-m %>% addPolygons(
-  fillColor = ~pal(density),
+map %>% addPolygons(
+  fillColor = ~pal(pa$coinsurance),
   weight = 2,
   opacity = 1,
   color = "white",
@@ -173,16 +174,13 @@ m %>% addPolygons(
 #Custom infos 
 #We'll generate the labels by handcrafting some HTML, and passing it to lapply(htmltools::HTML) so that Leaflet knows to treat each label as HTML instead of as plain text. We'll also set some label options to improve the style of the label element itself.
 labels <- sprintf(
-  paste0("<strong>", states$name, 
-         "</strong><br />Densitiy: ", states$density,
-         "<br /> Region: ", df$region,
-         "<br /> Having Diabetes Coinsurance mean: ", mean(hc$coinsurance),
-         "<br /> Businessyear: ", hc$BusinessYear
-         )
+ 
+         "<br /> Businessyear: ", pa$BusinessYear
+         
   )  %>% lapply(htmltools::HTML)
 
-m <- m %>% addPolygons(
-  fillColor = ~pal(density),
+xmap <- map %>% addPolygons(
+  fillColor = ~pal(pa$coinsurance),
   weight = 2,
   opacity = 1,
   color = "white",
@@ -199,9 +197,11 @@ m <- m %>% addPolygons(
     style = list("font-weight" = "normal", padding = "3px 8px"),
     textsize = "15px",
     direction = "auto"))
-m
+
+#illustrate the map with this line 
+map
 
 
 #add a legend. Because we chose to color our map using colorBin, the addLegend function makes it particularly easy to add a legend with the correct colors and intervals.
-m %>% addLegend(pal = pal, values = ~pal(density), opacity = 0.7, title = "amount of dollar",
+m %>% addLegend(pal = pal, values = ~pal(pa$coinsurance), opacity = 0.7, title = "amount of dollar",
                position = "bottomright")
